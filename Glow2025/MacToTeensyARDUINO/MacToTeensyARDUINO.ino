01b8daa42c2906043bdb6fb4c7777f165dbf8331
FeelinPeachyMate <an.frisch@mail.com>
2025-10-13 16:44:18 +0200

    feat: broadcast commands

--- Glow2025/MacToTeensyARDUINO/MacToTeensyARDUINO.ino
+++ Glow2025/MacToTeensyARDUINO/MacToTeensyARDUINO.ino
@@ -13,29 +13,35 @@ const int LED_PIN = LED_BUILTIN;
 
 struct DelayedCommand {
   String source;
-  String command;   // STAR_ARRIVED or CLIMAX_READY
-  unsigned long triggerTime; // millis() when it should be sent
+  String command;              // STAR_ARRIVED or CLIMAX_READY
+  unsigned long triggerTime;   // millis() when it should be sent
   bool active;
 };
 
 const int MAX_DELAYED = 10;
 DelayedCommand delayed[MAX_DELAYED];
 
+// =====================================================
 // Helper function to schedule delayed commands
+// =====================================================
 void scheduleDelayedCommand(String source, String command) {
   for (int i = 0; i < MAX_DELAYED; i++) {
     if (!delayed[i].active) {
       delayed[i].source = source;
       delayed[i].command = command;
-      delayed[i].triggerTime = millis() + random(5000, 8000); // 5–8 seconds
+      delayed[i].triggerTime = millis() + random(5000, 8000);  // 5–8 seconds
       delayed[i].active = true;
       return;
     }
   }
+
   // Overflow protection
   Serial.println("!!" + source + ":MASTER:REQUEST:ERROR:{too_many_delayed_commands}##");
 }
 
+// =====================================================
+// Setup
+// =====================================================
 void setup() {
   Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT);
@@ -46,6 +52,9 @@ void setup() {
   for (int i = 0; i < MAX_DELAYED; i++) delayed[i].active = false;
 }
 
+// =====================================================
+// Main loop
+// =====================================================
 void loop() {
   // --- Handle incoming serial data ---
   while (Serial.available() > 0) {
@@ -76,7 +85,7 @@ void loop() {
   for (int i = 0; i < MAX_DELAYED; i++) {
     if (delayed[i].active && now >= delayed[i].triggerTime) {
       sendConfirm(delayed[i].source, delayed[i].command);
-      delayed[i].active = false; // deactivate
+      delayed[i].active = false;  // deactivate
     }
   }
 }
@@ -104,6 +113,8 @@ void handleMessage(String msg) {
   }
 
   String source = msg.substring(0, c1);
+  if (source.startsWith("!")) source.remove(0, 1);
+
   String maybeType = msg.substring(c1 + 1, c2);
   String command = msg.substring(c2 + 1);
 
@@ -114,15 +125,29 @@ void handleMessage(String msg) {
   blinkLED();
 
   if (maybeType.equalsIgnoreCase("REQUEST")) {
+    String devices[] = {"ARM1", "ARM2", "ARM3", "ARM4", "ARM5", "CENTER", "TOP"};
+
     // Schedule delayed commands if needed
     if (command.equalsIgnoreCase("SEND_STAR")) {
-      scheduleDelayedCommand(source, "STAR_ARRIVED");
+      if (source.equalsIgnoreCase("BROADCAST")) {
+        for (int i = 0; i < 7; i++) scheduleDelayedCommand(devices[i], "STAR_ARRIVED");
+      } else {
+        scheduleDelayedCommand(source, "STAR_ARRIVED");
+      }
     } else if (command.equalsIgnoreCase("BUILDUP_CLIMAX_CENTER")) {
-      scheduleDelayedCommand(source, "CLIMAX_READY");
+      if (source.equalsIgnoreCase("BROADCAST")) {
+        for (int i = 0; i < 7; i++) scheduleDelayedCommand(devices[i], "CLIMAX_READY");
+      } else {
+        scheduleDelayedCommand(source, "CLIMAX_READY");
+      }
     }
 
     // Always send immediate confirm
-    sendConfirm(source, command);
+    if (source.equalsIgnoreCase("BROADCAST")) {
+      for (int i = 0; i < 7; i++) sendConfirm(devices[i], command);
+    } else {
+      sendConfirm(source, command);
+    }
   } else {
     Serial.println("!!" + source + ":MASTER:REQUEST:ERROR:{unknown_type_" + maybeType + "}##");
   }
@@ -145,57 +170,41 @@ void sendConfirm(String source, String command) {
 
   if (command.equalsIgnoreCase("MAKE_STAR")) {
     msg += "MAKE_STAR##";
-  }
-  else if (command.equalsIgnoreCase("SEND_STAR")) {
+  } else if (command.equalsIgnoreCase("SEND_STAR")) {
     msg += "SEND_STAR##";
-  }
-  else if (command.equalsIgnoreCase("CANCEL_STAR")) {
+  } else if (command.equalsIgnoreCase("CANCEL_STAR")) {
     msg += "CANCEL_STAR##";
-  }
-  else if (command.equalsIgnoreCase("STAR_ARRIVED")) {
+  } else if (command.equalsIgnoreCase("STAR_ARRIVED")) {
     msg += "STAR_ARRIVED{";
     msg += "SPEED=" + String(random(10, 100)) + ",";
     msg += "COLOR=" + String(random(0, 255)) + ",";
     msg += "BRIGHTNESS=" + String(random(50, 255)) + ",";
     msg += "SIZE=" + String(random(1, 10)) + "}##";
-  }
-  else if (command.equalsIgnoreCase("ADD_STAR")) {
+  } else if (command.equalsIgnoreCase("ADD_STAR")) {
     msg += "ADD_STAR##";
-  }
-  else if (command.equalsIgnoreCase("BUILDUP_CLIMAX_CENTER")) {
+  } else if (command.equalsIgnoreCase("BUILDUP_CLIMAX_CENTER")) {
     msg += "BUILDUP_CLIMAX_CENTER{SPEED=" + String(random(50, 200)) + "}##";
-  }
-  else if (command.equalsIgnoreCase("CLIMAX_READY")) {
+  } else if (command.equalsIgnoreCase("CLIMAX_READY")) {
     msg += "CLIMAX_READY##";
-  }
-  else if (command.equalsIgnoreCase("START_CLIMAX_CENTER")) {
+  } else if (command.equalsIgnoreCase("START_CLIMAX_CENTER")) {
     msg += "START_CLIMAX_CENTER{TIME=" + String(random(1000, 5000)) + "}##";
-  }
-  else if (command.equalsIgnoreCase("START_CLIMAX_TOP")) {
+  } else if (command.equalsIgnoreCase("START_CLIMAX_TOP")) {
     msg += "START_CLIMAX_TOP{TIME=" + String(random(1000, 5000)) + "}##";
-  }
-  else if (command.equalsIgnoreCase("STOP_CLIMAX_CENTER")) {
+  } else if (command.equalsIgnoreCase("STOP_CLIMAX_CENTER")) {
     msg += "STOP_CLIMAX_CENTER##";
-  }
-  else if (command.equalsIgnoreCase("STOP_CLIMAX_TOP")) {
+  } else if (command.equalsIgnoreCase("STOP_CLIMAX_TOP")) {
     msg += "STOP_CLIMAX_TOP##";
-  }
-  else if (command.equalsIgnoreCase("START_IDLE")) {
+  } else if (command.equalsIgnoreCase("START_IDLE")) {
     msg += "START_IDLE##";
-  }
-  else if (command.equalsIgnoreCase("STOP_IDLE")) {
+  } else if (command.equalsIgnoreCase("STOP_IDLE")) {
     msg += "STOP_IDLE##";
-  }
-  else if (command.equalsIgnoreCase("PING")) {
+  } else if (command.equalsIgnoreCase("PING")) {
     msg += "PING##";
-  }
-  else if (command.equalsIgnoreCase("RESET")) {
+  } else if (command.equalsIgnoreCase("RESET")) {
     msg += "RESET##";
-  }
-  else if (command.equalsIgnoreCase("COMM_ERROR")) {
+  } else if (command.equalsIgnoreCase("COMM_ERROR")) {
     msg += "COMM_ERROR{STRING=Random_Comm_Error_" + String(random(100, 999)) + "}##";
-  }
-  else {
+  } else {
     msg = "!!" + source + ":MASTER:REQUEST:ERROR:{unknown_command_" + command + "}##";
   }
 
